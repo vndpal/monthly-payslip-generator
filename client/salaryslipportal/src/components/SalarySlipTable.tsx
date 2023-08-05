7b3551ca8a4bf7623c543182a7b52d42c81679cf
Vinod Pal <undervinod@gmail.com>
2023-08-05 16:35:18 +0530

    Added toFixed(2) to render to decimal points in the client application

--- client/salaryslipportal/src/components/SalarySlipTable.tsx
+++ client/salaryslipportal/src/components/SalarySlipTable.tsx
@@ -13,6 +13,7 @@ interface SalarySlipProps {
 }
 
 const SalarySlipComponent: React.FC<SalarySlipProps> = ({ payslipData }) => {
+  console.log(payslipData);
   return (
     <div className="salary-slip">
       <h2>Salary Slip</h2>
@@ -28,19 +29,19 @@ const SalarySlipComponent: React.FC<SalarySlipProps> = ({ payslipData }) => {
           </tr>
           <tr>
             <td className="label">Gross Income:</td>
-            <td>{payslipData.grossIncome}</td>
+            <td>{payslipData.grossIncome.toFixed(2)}</td>
           </tr>
           <tr>
             <td className="label">Income Tax:</td>
-            <td>{payslipData.incomeTax}</td>
+            <td>{payslipData.incomeTax.toFixed(2)}</td>
           </tr>
           <tr>
             <td className="label">Net Income:</td>
-            <td>{payslipData.netIncome}</td>
+            <td>{payslipData.netIncome.toFixed(2)}</td>
           </tr>
           <tr>
             <td className="label">Super:</td>
-            <td>{payslipData.super}</td>
+            <td>{payslipData.super.toFixed(2)}</td>
           </tr>
         </tbody>
       </table>
