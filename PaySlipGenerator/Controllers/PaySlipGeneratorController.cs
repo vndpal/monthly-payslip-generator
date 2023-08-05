543e2bb52d70e10fdaf0877b76cbface15e5bd7a
Vinod Pal <undervinod@gmail.com>
2023-08-05 19:16:33 +0530

    Added versioning to the api endpoints

--- PaySlipGenerator/Controllers/PaySlipGeneratorController.cs
+++ PaySlipGenerator/Controllers/PaySlipGeneratorController.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 
 namespace PaySlipGenerator.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PaySlipGeneratorController : ControllerBase
     {
