7d49b1736f48b7aa106af98e6ec229958219f839
Vinod Pal <undervinod@gmail.com>
2023-08-05 23:15:25 +0530

    Added Swagger UI in deployed version so that API documenation can be seen

--- PaySlipGenerator/Program.cs
+++ PaySlipGenerator/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
